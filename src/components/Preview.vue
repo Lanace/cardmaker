89c0e1fb341fc2f72a2475264f14dca08a54439c
JeonIlju <lanace93@gmail.com>
2018-08-28 18:09:08 +0900

    캔버스에 이미지 로드 기능 추가

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,7 +1,8 @@
 <template>
   <section>
     <h2>Preview</h2>
-    <canvas></canvas>
+    <input id="file-input" type="file" v-on:change="fileLoad"/>
+    <canvas id="canvas"></canvas>
   </section>
 </template>
 
@@ -9,7 +10,31 @@
 export default {
   data () {
     return {
-      form: {}
+      form: {},
+      filePath: '',
+      img: {},
+      fr: {}
+    }
+  },
+  methods: {
+    fileLoad: function (event) {
+      var file = event.target.files[0]
+      this.fr = new FileReader()
+      this.fr.onload = this.createImage
+      this.fr.readAsDataURL(file)
+    },
+    createImage: function () {
+      this.img = new Image()
+      this.img.onload = this.imageLoaded
+      this.img.src = this.fr.result
+    },
+    imageLoaded: function () {
+      var canvas = document.getElementById('canvas')
+      canvas.width = this.img.width
+      canvas.height = this.img.height
+      var ctx = canvas.getContext('2d')
+      ctx.drawImage(this.img, 0, 0)
+      alert(canvas.toDataURL('image/png'))
     }
   }
 }
